1b881bdf0bdd89bf574942250abe7f17180180f8
lhorla <teteelola@gmail.com>
2021-04-14 23:30:58 +0100

    another update

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function mobileMenu() {
   toggleButton.classList.toggle("active");
   navLinks.classList.toggle("active");
 }
+
+const navLinks = document.querySelectorAll(".nav-links");
+
+navLinks.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu () {
+  toggleButton.classList.remove("active");
+  navLinks.classList.remove("active");
+}
